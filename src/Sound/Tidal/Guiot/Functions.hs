1d4f6de1016c99efee00a9b2261d555d56be7787
mxmxyz <mxmxyzgxyzt@gmail.com>
2019-03-10 15:52:14 +0100

    yes, y, gtfo, g

--- src/Sound/Tidal/Guiot/Functions.hs
+++ src/Sound/Tidal/Guiot/Functions.hs
@@ -6,3 +6,9 @@ import Sound.Tidal.Chords
 
 --courtesy of yaxu
 wrap s e p = (p |% (e - s)) |+ s
+
+--yes and gtfo (n was taken)
+yes = degradeBy 0
+y = degradeBy 0
+gtfo = degradeBy 1
+g = degradeBy 1
