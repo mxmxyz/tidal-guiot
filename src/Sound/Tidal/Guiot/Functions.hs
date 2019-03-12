975f8551736bd2ec75bfdd326ab3d0818c6b1606
mxmxyz <mxmxyzgxyzt@gmail.com>
2019-03-12 13:54:23 +0100

    removed speedUp as it was a duplicate of inside

--- src/Sound/Tidal/Guiot/Functions.hs
+++ src/Sound/Tidal/Guiot/Functions.hs
@@ -15,6 +15,3 @@ g = degradeBy 1
 
 --shrand, to allow for non-syncing randomness
 shrand n = (fast (1.453^n) $ rand)
-
---speedUp, apply 'fast' only to a series of functions (eg whenmod, spaceOut)
-speedUp n functs = fast n . functs . slow n 
