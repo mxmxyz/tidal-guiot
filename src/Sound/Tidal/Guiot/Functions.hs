26b14591bb8fe2a9990fd0943843036ec4183223
mxmxyz <mxmxyzgxyzt@gmail.com>
2019-05-20 10:32:35 +0200

    shift, transpose, degradeBy aliases

--- src/Sound/Tidal/Guiot/Functions.hs
+++ src/Sound/Tidal/Guiot/Functions.hs
@@ -4,17 +4,33 @@ import Sound.Tidal.Context
 import Sound.Tidal.Scales
 import Sound.Tidal.Chords
 
---courtesy of yaxu
+--wrap, courtesy of @yaxu
 wrap s e p = (p |% (e - s)) |+ s
-
---yes and gtfo (n was taken)
-yes = degradeBy 0
-y = degradeBy 0
-gtfo = degradeBy 1
-g = degradeBy 1
 
 --shrand, to allow for non-syncing randomness
 shrand n = (fast (1.453^n) $ rand)
 
 --scaleshift, to use a scale and +/- by n degrees 
 shiftscale scale n p = scale ((+ n) <$> p)
+
+--shiftl and shiftr (clearer <~/~>)
+shiftl n p = (n <~) $ p
+shiftr n p = (n ~>) $ p
+
+--degradeBy shorthands, courtesy @kindohm
+degBy = degradeBy
+deg0 = degBy 0.0
+deg1 = degBy 0.1
+deg2 = degBy 0.2
+deg3 = degBy 0.3
+deg4 = degBy 0.4
+deg5 = degBy 0.5
+deg6 = degBy 0.6
+deg7 = degBy 0.7
+deg8 = degBy 0.8
+deg9 = degBy 0.9
+mute = degBy 1
+
+--transup and transdown
+transup n p = (|+ note n) $ p
+transdown n p = (|- note n) $ p
